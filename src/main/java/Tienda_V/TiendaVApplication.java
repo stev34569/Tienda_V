2401d9efa5e700e206137613ee36ba1b7ca50bb9
steve <steve@Alonso>
2022-05-20 21:38:15 -0600

    Creacion de Proyecto tienda en la cuenta git

---
+++ src/main/java/Tienda_V/TiendaVApplication.java
@@ -0,0 +1,13 @@
+package Tienda_V;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaVApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaVApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tienda_V/TiendaVApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda_V;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaVApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
